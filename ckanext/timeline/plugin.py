56836ba018a7258bfb6487ee56bc2e35601aa72f
Mikael Karlsson <i8myshoes@gmail.com>
2014-08-20 13:38:32 +0300

    Use extras_ fields

--- ckanext/timeline/plugin.py
+++ ckanext/timeline/plugin.py
@@ -70,7 +70,7 @@ def timeline(context, request_data):
 
     rl = []
     for s, e, m in ls:
-        r = solr.query('TempCoverageBegin:[* TO {e}] AND TempCoverageEnd:[{s} TO *]'.format(s=s, e=e),
+        r = solr.query('extras_TempCoverageBegin:[* TO {e}] AND extras_TempCoverageEnd:[{s} TO *]'.format(s=s, e=e),
                        fields=['id'],
                        rows=0)
         rl.append((s, e, m, r._numFound))
