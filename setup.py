0c6ad33a9b970a61f13d6341dcadc2aed07da938
Mikael Karlsson <i8myshoes@gmail.com>
2015-12-07 16:01:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='ckanext-timeline',
-    version='0.2.1',
-    description="CKAN extension for timeline facet",
+    version='0.2.2',
+    description='CKAN extension for timeline facet',
     long_description=
-    """
-    """,
+    '''
+    ''',
     classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='ckan ckanext timeline facet',
     author='Mikael Karlsson',
     author_email='i8myshoes@gmail.com',
-    url='https://github.com/B2FIND/ckanext-timeline',
+    url='https://github.com/EUDAT-B2FIND/ckanext-timeline',
     license='AGPLv3',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.timeline'],
@@ -21,8 +21,8 @@ setup(
         # -*- Extra requirements: -*-
     ],
     entry_points=
-    """
+    '''
     [ckan.plugins]
     timeline=ckanext.timeline.plugin:TimelinePlugin
-    """,
+    ''',
 )
